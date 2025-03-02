cdf9c6c7e5e6512e97b1199517ade99475397caa
kamaoji <karun73364@gmail.com>
2025-03-02 18:58:30 +0530

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,16 @@
-// Firebase Configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDqzbS0CeWAn8jeRF1E4CpKW25uH58DsMs",
-    authDomain: "telegramearningbot.firebaseapp.com",
-    projectId: "telegramearningbot",
-    storageBucket: "telegramearningbot.firebasestorage.app",
-    messagingSenderId: "883236360921",
-    appId: "1:883236360921:web:aa79b5bbb64892f6e2a4c8",
-    measurementId: "YOUR_MEASUREMENT_ID"
-};
-
 // Firebase Initialize
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDqzbS0CeWAn8jeRF1E4CpKW25uH58DsMs",
+  authDomain: "telegramearningbot.firebaseapp.com",
+  databaseURL: "https://your-project-id.firebaseio.com",
+  projectId: "telegramearningbot",
+  storageBucket: "telegramearningbot.firebasestorage.app",
+  messagingSenderId: "883236360921",
+  appId: "1:883236360921:web:aa79b5bbb64892f6e2a4c8"
+};
+
+firebase.initializeApp(firebaseConfig);
