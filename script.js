ca3849d6b7f4e1677bfdf73ab06ad85bf87b7ff6
kamaoji <karun73364@gmail.com>
2025-03-03 15:26:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ document.getElementById("spinButton").addEventListener("click", function() {
     let randomPrize = prizes[Math.floor(Math.random() * prizes.length)];
     document.getElementById("result").innerText = "आपका इनाम: " + randomPrize;
 });
+
+document.getElementById("checkJoin").addEventListener("click", function() {
+    let userId = "123456"; // यहां Telegram User ID आएगी
+
+    fetch("https://telegram-earning-mini-app.onrender.com/check_join", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ user_id: userId })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.joined) {
+            document.getElementById("result").innerText = "आपने चैनल जॉइन कर लिया है!";
+        } else {
+            document.getElementById("result").innerText = "कृपया पहले चैनल जॉइन करें!";
+        }
+    });
+});
